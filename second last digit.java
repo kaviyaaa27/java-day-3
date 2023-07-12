4e21d4ec4f9d6764f7eeae73f28f49fb00c3ec05
kaviyaaa27 <113441605+kaviyaaa27@users.noreply.github.com>
2023-07-12 19:31:20 +0530

    Create second last digit.java

---
+++ second last digit.java
@@ -0,0 +1,7 @@
+public class MyClass {
+    public static void main(String args[]) {
+        int x=202106001;
+      int secondlast_digit = (x%100)/10;
+      System.out.println(secondlast_digit + " is the second last digit");
+    }
+}
