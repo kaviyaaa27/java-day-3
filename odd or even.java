3c2105b4e595fed914ef0925320ed234bb7d2f49
kaviyaaa27 <113441605+kaviyaaa27@users.noreply.github.com>
2023-07-12 19:33:47 +0530

    Create odd or even.java

---
+++ odd or even.java
@@ -0,0 +1,18 @@
+public class MyClass {
+    public static void main(String args[]) {
+      int N=10;
+      if (N % 2 == 0) {
+      System.out.println(N + " is even ");
+      }
+      
+      else if (N == 0) {
+          System.out.println(N +"is zero");
+      }
+      
+      else {
+          System.out.println(N + "is odd");
+          
+      }
+    }
+}
+      
