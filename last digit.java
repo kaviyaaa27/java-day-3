a7790604affc9932bd138969140c6fed6a304764
kaviyaaa27 <113441605+kaviyaaa27@users.noreply.github.com>
2023-07-12 19:32:13 +0530

    Create last digit.java

---
+++ last digit.java
@@ -0,0 +1,7 @@
+public class MyClass {
+    public static void main(String args[]) {
+      int x=202106001;
+      int last_digit = x%10;
+      System.out.println(last_digit + " is the last digit");
+    }
+}
