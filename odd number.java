1a0ff0b0f8533cf25f2d0b64ec7e26948b4b5fc5
kaviyaaa27 <113441605+kaviyaaa27@users.noreply.github.com>
2023-07-12 19:33:11 +0530

    Create odd number.java

---
+++ odd number.java
@@ -0,0 +1,14 @@
+public class MyClass {
+    public static void main(String args[]) {
+        int x = 3;
+        if(x%2==1) {
+            System.out.println("The number is odd");
+            if(x%2==1) {
+                System.out.println("2");
+            }
+            else {
+                System.out.println("1");
+            }
+        }
+    }
+}
